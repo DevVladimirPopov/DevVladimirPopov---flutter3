d420628d1431ca562c2c2b3a3c794bbbbd20ae1c
DevVladimirPopov <107247047+DevVladimirPopov@users.noreply.github.com>
2022-06-20 09:50:08 +0100

    done

--- app_new/lib/main.dart
+++ app_new/lib/main.dart
@@ -1,4 +1,8 @@
+// Flutter imports:
 import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
@@ -16,12 +20,25 @@ class Home extends StatelessWidget {
         title: Text('Home work 3.6'),
       ),
       body: Center(
-        child: SvgPicture.asset(
-          'assets/images/gotmyvaccinesticker.svg',
-          width: 200.0,
-          height: 200.0,
-        ),
-      ),
+          child: CarouselSlider(
+        options: CarouselOptions(height: 400.0),
+        items: [1, 2, 3, 4, 5].map((i) {
+          return Builder(
+            builder: (BuildContext context) {
+              return Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.symmetric(horizontal: 5.0),
+                  decoration:
+                      BoxDecoration(color: Color.fromARGB(255, 90, 117, 209)),
+                  child: SvgPicture.asset(
+                    'assets/images/gotmyvaccinesticker.svg',
+                    width: 200.0,
+                    height: 200.0,
+                  ));
+            },
+          );
+        }).toList(),
+      )),
     );
   }
 }
